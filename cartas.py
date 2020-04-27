b53e775a375f56a30905873560a276229617c15f
DCorderoR <corderorodriguezdavid79@gmail.com>
2020-04-27 22:15:58 +0200

    Creación modulo

---
+++ cartas.py
@@ -0,0 +1,12 @@
+palos = ('copas', 'bastos', 'espadas', 'oros')
+cartas = ('A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R')
+
+
+def creaBaraja():
+    baraja = []
+    for palo in palos:
+        for carta in cartas:
+            naipe = carta + palo
+            baraja.append(naipe)
+
+    return baraja
--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-palos = ('copas', 'bastos', 'espadas', 'oros')
-cartas = ('A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R')
-baraja = []
+import cartas
 
-
-def creaBaraja():
-    baraja = []
-    for palo in palos:
-        for carta in cartas:
-            naipe = carta + palo
-            baraja.append(naipe)
-
-    return baraja
-
-
-baraja1 = creaBaraja()
-baraja2 = creaBaraja()
-
-print(baraja1)
+print(cartas.creaBaraja())
