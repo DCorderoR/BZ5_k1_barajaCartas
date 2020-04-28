904fb316e6797318cea4077e5bf11d64be098748
DCorderoR <corderorodriguezdavid79@gmail.com>
2020-04-29 00:27:33 +0200

    clase baraja

--- cartas.py
+++ cartas.py
@@ -1,39 +1,49 @@
 import random
-palos = ('copas', 'bastos', 'espadas', 'oros')
-cartas = ('A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R')
 
 
-def creaBaraja():
-    baraja = []
-    for palo in palos:
-        for carta in cartas:
-            naipe = carta + palo
-            baraja.append(naipe)
+class Baraja():
+    palos = ('copas', 'bastos', 'espadas', 'oros')
+    cartas = ('A', '2', '3', '4', '5', '6', '7', 'S', 'C', 'R')
 
-    return baraja
+    def __init__(self):
+        self.__creaBaraja()
+
+    def __creaBaraja(self):
+        self.naipes = []
+        self.mano = []
+        for palo in self.palos:
+            for carta in self.cartas:
+                naipe = carta + palo
+                self.naipes.append(naipe)
+
+    def mezclar(self):
+        br = []
+        while len(self.naipes) != len(br):
+            n = random.randint(0, len(self.naipes)-1)
+            while self.naipes[n] in br:
+                n = random.randint(0, len(self.naipes)-1)
+            br.append(self.naipes[n])
+        self.naipes[:] = br
+
+    def repartir(self, players, cards):
+        for p in range(players):
+            self.mano.append([])
+
+        for ic in range(cards):
+            for ij in range(players):
+                carta = self.naipes.pop(0)
+                self.mano[ij].append(carta)
+
+    def invertir(self):
+        for i in range(len(self.naipes)//2):
+            aux = self.naipes[i]
+            self.naipes[i] = self.naipes[-1-i]
+            self.naipes[-1-i] = aux
+
+    def recoger(self):
+        self.__creaBaraja()
 
 
-def mezclar(b):
-    br = []
-    i = 0
-    while i < 40:
-        n = random.randint(0, 39)
-        while b[n] in br:
-            n = random.randint(0, 39)
-        br.append(b[n])
-        i += 1
-    b = br
-    return b
-
-
-def repartir(b, players, cards):
-    res = []
-    for p in range(players):
-        res.append([])
-
-    for ic in range(cards):
-        for ij in range(players):
-            carta = b.pop(0)
-            res[ij].append(carta)
-
-    return res
+if __name__ == '__main__':
+    b = Baraja()
+    b.mezclar()
--- main.py
+++ main.py
@@ -1,6 +1,2 @@
 import cartas
 
-b = cartas.creaBaraja()
-manos = cartas.repartir(b, 3, 5)
-
-print(manos)
